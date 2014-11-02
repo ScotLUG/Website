36c2b18d308ccaaac418bb97f82131f739f4abe4
David Jones <david.jones@fmail.co.uk>
2014-11-02 18:39:58 +0000

    Clean up views
    Title and page header helpers were confusing and didn't add much, so
    they were removed, and the page header moved into the template.
    
    A nav_item_link_tag helper has been added, which nests a link within a
    list item with an "active" class when current.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,15 +1,10 @@
 module ApplicationHelper
-  # Returns a <title> element and provides content for :title.
-  # :page_header is included in the tag content, if it is defined.
-  def title(content)
-    provide :title, content
-    content_tag :title, [content_for(:page_header), content].compact.join(' - ')
-  end
-
-  # Returns a <h1 class="page-header"> Bootstrap element and provides
-  # content for :page_header.
-  def page_header(content)
-    provide :page_header, content
-    content_tag :h1, content, class: 'page-header'
+  # Creates a link tag of the given +name+ using a URL created by the
+  # set of +options+, wrapped in a +<li>+ tag.  The wrapping tag is
+  # given a class of "active" if the URL is that of the current
+  # page.
+  def nav_item_link_tag(name, options)
+    html_class = 'active' if current_page?(options)
+    content_tag :li, link_to(name, options), class: html_class
   end
 end
